8a93fd5e9b02a55cf3974abb02958d41026fa98b
amol pawar <amoljp19@gmail.com>
2021-02-05 17:30:24 +0530

    added new user data activity opening code and saving data on return back

--- app/src/main/java/com/softaai/usersdataapp/usersdata/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/usersdataapp/usersdata/ui/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.softaai.usersdataapp.usersdata.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.softaai.usersdataapp.R
+import com.softaai.usersdataapp.model.Data
 import com.softaai.usersdataapp.usersdata.adapter.UsersListAdapter
+import com.softaai.usersdataapp.usersdata.viewmodel.UsersDataViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val usersDataViewModel : UsersDataViewModel by viewModels()
+
+    private val newUserDataActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +29,31 @@ class MainActivity : AppCompatActivity() {
         val adapter = UsersListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        usersDataViewModel.allUsers.observe(this, Observer { users ->
+            users?.let { adapter.submitList(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewUserDataActivity::class.java)
+            startActivityForResult(intent, newUserDataActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newUserDataActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewUserDataActivity.EXTRA_REPLY)?.let {
+                val user = Data(it, "", "", 1, "")
+                usersDataViewModel.insert(user)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
