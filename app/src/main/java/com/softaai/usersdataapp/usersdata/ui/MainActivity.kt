11611fa604f6c2faaa3cdd1d09abe18b5a47be10
amol pawar <amoljp19@gmail.com>
2021-02-05 16:05:39 +0530

    added recyclerview inflating and initialising code in main activity

--- app/src/main/java/com/softaai/usersdataapp/usersdata/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/usersdataapp/usersdata/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.softaai.usersdataapp.usersdata.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.softaai.usersdataapp.R
+import com.softaai.usersdataapp.usersdata.adapter.UsersListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = UsersListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
