3095f5a596992b4cc5f6c3c39262a94c46ee9b57
amol pawar <amoljp19@gmail.com>
2021-02-05 16:37:43 +0530

    added database callback

--- app/src/main/java/com/softaai/usersdataapp/UsersDataApp.kt
+++ app/src/main/java/com/softaai/usersdataapp/UsersDataApp.kt
@@ -1,9 +1,16 @@
 package com.softaai.usersdataapp
 
 import android.app.Application
+import com.softaai.usersdataapp.persistence.UsersDataRoomDb
+import com.softaai.usersdataapp.repository.UsersDataRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 @HiltAndroidApp
 class UsersDataApp : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
 
+    val database by lazy { UsersDataRoomDb.getUsersDatabase(this, applicationScope) }
+    val repository by lazy { UsersDataRepository(database.usersDataDao()) }
 }
--- app/src/main/java/com/softaai/usersdataapp/persistence/UsersDataRoomDb.kt
+++ app/src/main/java/com/softaai/usersdataapp/persistence/UsersDataRoomDb.kt
@@ -4,26 +4,50 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.softaai.usersdataapp.model.Data
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Data::class), version = 1, exportSchema = false)
 abstract class UsersDataRoomDb : RoomDatabase() {
 
     abstract fun usersDataDao(): UsersDataApiResponseDao
 
+    private class UsersDatabaseCallback(
+            private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    val usersDao = database.usersDataDao()
+
+                    usersDao.deleteAll()
+
+
+                    val data = Data("", "", "Amol", 1,"Pawar")
+                    usersDao.insert(data)
+                }
+            }
+        }
+    }
+
     companion object {
 
         @Volatile
         private var INSTANCE: UsersDataRoomDb? = null
 
-        fun getUsersDatabase(context: Context): UsersDataRoomDb {
+        fun getUsersDatabase(context: Context, scope: CoroutineScope): UsersDataRoomDb {
 
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     UsersDataRoomDb::class.java,
                     "users_database"
-                ).build()
+                ).addCallback(UsersDatabaseCallback(scope))
+                        .build()
                 INSTANCE = instance
 
                 instance
