8e9faea597e46c3fbdbf6b057f57d0114a29dc31
amol pawar <amoljp19@gmail.com>
2021-02-05 15:07:58 +0530

    passes usersDataApiClient in repository as parameter

--- app/src/main/java/com/softaai/usersdataapp/usersdata/viewmodel/UsersDataViewModel.kt
+++ app/src/main/java/com/softaai/usersdataapp/usersdata/viewmodel/UsersDataViewModel.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.softaai.usersdataapp.model.Data
+import com.softaai.usersdataapp.network.UsersDataApiClient
 import com.softaai.usersdataapp.repository.UsersDataRepository
 import kotlinx.coroutines.launch
 
-class UsersDataViewModel(private val usersDataRepository: UsersDataRepository) : ViewModel() {
+class UsersDataViewModel(private val usersDataApiClient: UsersDataApiClient, private val usersDataRepository: UsersDataRepository) : ViewModel() {
 
     val allUsers: LiveData<List<Data>> = usersDataRepository.allUsers.asLiveData()
 
