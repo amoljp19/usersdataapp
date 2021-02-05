274e05677d2dc35c68f919393f3832093a423c3c
amol pawar <amoljp19@gmail.com>
2021-02-05 13:00:34 +0530

    added lifecycle components dependencies and user data viewmodel

---
+++ app/src/main/java/com/softaai/usersdataapp/usersdata/viewmodel/UsersDataViewModel.kt
@@ -0,0 +1,19 @@
+package com.softaai.usersdataapp.usersdata.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.softaai.usersdataapp.model.Data
+import com.softaai.usersdataapp.repository.UsersDataRepository
+import kotlinx.coroutines.launch
+
+class UsersDataViewModel(private val usersDataRepository: UsersDataRepository) : ViewModel() {
+
+    val allUsers: LiveData<List<Data>> = usersDataRepository.allUsers.asLiveData()
+
+
+    fun insert(data: Data) = viewModelScope.launch {
+        usersDataRepository.insert(data)
+    }
+}
