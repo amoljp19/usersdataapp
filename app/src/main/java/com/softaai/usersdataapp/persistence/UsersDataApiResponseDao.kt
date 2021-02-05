5f9cacf8d036dc6c80cfecf2a8c43e8e824f90f8
amol pawar <amoljp19@gmail.com>
2021-02-05 11:24:45 +0530

    wrapped list of data into coroutine flow

--- app/src/main/java/com/softaai/usersdataapp/persistence/UsersDataApiResponseDao.kt
+++ app/src/main/java/com/softaai/usersdataapp/persistence/UsersDataApiResponseDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.softaai.usersdataapp.model.Data
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsersDataApiResponseDao {
 
     @Query("SELECT * FROM Data ORDER BY firstName ASC")
-    fun getAlphabetizedUsersData(): List<Data>
+    fun getAlphabetizedUsersData(): Flow<List<Data>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(data: Data)
